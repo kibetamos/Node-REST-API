84e6f867a48ae13f95a3255f6a68ec9e86938ff1
kibet kilel <amos.rotich@strathmore.edu>
2021-07-23 17:20:37 +0300

    update on controller and rourte files

--- services/posts.service.js
+++ services/posts.service.js
@@ -1,1 +1,15 @@
-posts.service.js
+const db = require("../config/db.config");
+
+exports.addPost = (data, callback) => {
+  db.query(
+    `INSERT INTO posts (description, imagePath, datetimeCreated, addedByUserId)
+    VALUES (?, ?, ?, ?)`,
+    [data.description, data.imagePath, new Date(), data.addedByUserId],
+    (error, results, fields) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, "Post added successfully");
+    }
+  );
+};
